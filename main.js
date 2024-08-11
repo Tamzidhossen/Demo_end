e32afd1a1c8e4f83580b0669d7626cad2da22fe1
Tamzid Hossen <105188327+Tamzidhossen@users.noreply.github.com>
2024-08-11 21:51:41 +0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("submitButton").onclick = function(){
+  a = document.getElementById("aTextBox").value;
+  a = Number(a);
+  b = document.getElementById("bTextBox").value;
+  b = Number(b);
+  c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+  document.getElementById("cLabel").innerHTML = "Side C: " + c;
+}
